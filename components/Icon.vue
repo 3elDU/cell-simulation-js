5433131a9053818f6286196d6703ca68fb559970
Zakhar Voloshchuk (3elDU) <3elduu@gmail.com>
2023-06-01 15:50:43 +0300

    fix icon loading

--- components/Icon.vue
+++ components/Icon.vue
@@ -30,13 +30,10 @@ const cache = new Map();
 
 async function getSvgIconText(name: string): Promise<string> {
   if (!cache.has(name)) {
-    // Fetch icon from server
-    const [response, error] = await fetch(`icons/${name}.svg`)
-      .then(async (response) => {
-        return [await response.text(), null];
-      })
-      .catch((err) => {
-        return [`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    const { data: response, error: error } = await useFetch(`/icons/${name}.svg`);
+
+    if (error.value) {
+      return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
         <defs>
           <path id="icTwotoneHourglassTop0" fill="currentColor" d="m8 7.5l4 4l4-4V4H8z" opacity=".3" />
         </defs>
@@ -44,21 +41,19 @@ async function getSvgIconText(name: string): Promise<string> {
         <use href="#icTwotoneHourglassTop0" opacity=".3" />
         <path fill="currentColor"
           d="M18 2H6v6l4 4l-3.99 4.01L6 22h12l-.01-5.99L14 12l4-3.99V2zm-2 14.5V20H8v-3.5l4-4l4 4zm0-9l-4 4l-4-4V4h8v3.5z" />
-      </svg>`, err]
-      });
-
-    if (!error) {
-      cache.set(name, response);
+      </svg>`
+    } else {
+      const string = await (response.value as Blob).text();
+      cache.set(name, string);
+      console.log(string);
+      return string;
     }
-    return response;
   }
   return cache.get(name);
 }
 </script>
 
 <script setup lang="ts">
-import nuxtConfig from '~/nuxt.config';
-
 const props = defineProps({
   name: {
     type: String,
