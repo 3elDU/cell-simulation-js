385b1be205cb816e407c87aad7fc8f50aee7ab3d
Zakhar Voloshchuk (3elDU) <3elduu@gmail.com>
2023-06-01 15:31:55 +0300

    tell nuxt about the base directory, so it will deploy correctly

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  app: {
+    baseURL: "/cell-simulation-js",
+  },
   modules: ['@nuxtjs/tailwindcss'],
 })
